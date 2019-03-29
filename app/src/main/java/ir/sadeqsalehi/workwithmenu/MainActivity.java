35a0f74825ff4b2607735d9e42cc6fd2a453ac79
sadeqsalehi <sadeqsalehi@gmail.com>
2019-03-29 09:37:14 +0430

    Create menu.xml and inflate

--- app/src/main/java/ir/sadeqsalehi/workwithmenu/MainActivity.java
+++ app/src/main/java/ir/sadeqsalehi/workwithmenu/MainActivity.java
@@ -1,7 +1,8 @@
 package ir.sadeqsalehi.workwithmenu;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //options menu
+        // 1 - Programmatically
+        // 2 - Resource menu file
+
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        /*MenuInflater menuInflater = new MenuInflater(getApplicationContext());
+        menuInflater.inflate(R.menu.menu_main,menu); */
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+
+        return super.onCreateOptionsMenu(menu);
     }
 }
